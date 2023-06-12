c609f239d97622b00e256c799b917f1de0c9aba4
yiminwook <grs0416@gmail.com>
2023-06-12 22:14:02 +0900

    Feat: button click event test

--- counter/src/App/App.test.tsx
+++ counter/src/App/App.test.tsx
@@ -1,6 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "@/App/App";
-import { wait } from "@testing-library/user-event/dist/utils";
 
 describe("counter test", () => {
   test("counter started at 0", () => {
@@ -9,15 +8,32 @@ describe("counter test", () => {
     expect(counterElement).toHaveTextContent("0");
   });
 
-  test("minus button existed test", () => {
+  test("minus button existed", () => {
     render(<App />);
     const minusButtonElement = screen.getByTestId("minus-button");
     expect(minusButtonElement).toHaveTextContent("-");
   });
 
-  test("plus button existed test", () => {
+  test("plus button existed", () => {
     render(<App />);
     const plusButtonElement = screen.getByTestId("plus-button");
     expect(plusButtonElement).toHaveTextContent("+");
+  });
+
+  test("when minus button click, counter change to -1", async () => {
+    render(<App />);
+    const counterElement = screen.getByTestId("counter");
+    const minusButtonElement = screen.getByTestId("minus-button");
+    fireEvent.click(minusButtonElement);
+    expect(counterElement).toHaveTextContent("-1");
+  });
+
+  test("when plus button click twice, counter change to 2", () => {
+    render(<App />);
+    const counterElement = screen.getByTestId("counter");
+    const plusButtonElement = screen.getByTestId("plus-button");
+    fireEvent.click(plusButtonElement);
+    fireEvent.click(plusButtonElement);
+    expect(counterElement).toHaveTextContent("2");
   });
 });
--- counter/src/App/App.tsx
+++ counter/src/App/App.tsx
@@ -3,11 +3,24 @@ import { useState } from "react";
 
 const App = () => {
   const [conuter, setCounter] = useState(0);
+  const handleMinus = () => {
+    setCounter((pre) => pre - 1);
+  };
+
+  const handlePlus = () => {
+    setCounter((pre) => pre + 1);
+  };
 
   return (
     <div className="App">
       <main>
         <h3 data-testid="counter">{conuter}</h3>
+        <button data-testid="minus-button" onClick={handleMinus}>
+          -
+        </button>
+        <button data-testid="plus-button" onClick={handlePlus}>
+          +
+        </button>
       </main>
     </div>
   );
