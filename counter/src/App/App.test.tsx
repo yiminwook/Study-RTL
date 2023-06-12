9653ff700c478545305d0530285bc076ef92c4ee
yiminwook <grs0416@gmail.com>
2023-06-12 21:57:11 +0900

    Feat: button exist test

--- counter/src/App/App.test.tsx
+++ counter/src/App/App.test.tsx
@@ -1,10 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "@/App/App";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 describe("counter test", () => {
-  test("counter starts at 0", () => {
+  test("counter started at 0", () => {
     render(<App />);
     const counterElement = screen.getByTestId("counter");
     expect(counterElement).toHaveTextContent("0");
+  });
+
+  test("minus button existed test", () => {
+    render(<App />);
+    const minusButtonElement = screen.getByTestId("minus-button");
+    expect(minusButtonElement).toHaveTextContent("-");
+  });
+
+  test("plus button existed test", () => {
+    render(<App />);
+    const plusButtonElement = screen.getByTestId("plus-button");
+    expect(plusButtonElement).toHaveTextContent("+");
   });
 });
