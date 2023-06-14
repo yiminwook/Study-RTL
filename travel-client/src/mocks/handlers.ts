6ae07983bc77c5a471712a50735c0c5474be2cc1
yiminwook <grs0416@gmail.com>
2023-06-14 18:47:28 +0900

    Add: msw handlers and server

---
+++ travel-client/src/mocks/handlers.ts
@@ -0,0 +1,32 @@
+import { rest } from "msw";
+
+const handlers = [
+  rest.get("http://localhost:5000/products", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          name: "America",
+          imagePath: "/images/america.jpeg",
+        },
+        {
+          name: "England",
+          imagePath: "/images/england.jpeg",
+        },
+      ])
+    );
+  }),
+  rest.get("http://localhost:5000/options", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          name: "Insurance",
+        },
+        {
+          name: "Dinner",
+        },
+      ])
+    );
+  }),
+];
+
+export default handlers;
--- travel-client/src/setupTests.ts
+++ travel-client/src/setupTests.ts
@@ -2,4 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import mockServer from "@/mocks/server";
+
+//테스트 전에
+beforeAll(() => mockServer.listen());
+//테스트가 끝난후
+afterEach(() => mockServer.resetHandlers());
+//모든 테스트가 끝난후
+afterAll(() => mockServer.close());
